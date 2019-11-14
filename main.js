79cf4ee5a7a824d4631b33c5afaf00b1f74ff7ac
Jed Hartley <hartleje@oregonstate.edu>
2019-11-13 21:20:35 -0800

    removed API because my popup stopped working

--- main.js
+++ main.js
@@ -3,33 +3,3 @@ function updateView(){
     alert("Zagster Data")
 }
 
-require('dotenv').config()
-const express = require('express')
-const { Pool } = require('pg')
-const app = express()
-const PORT = process.env.PORT || 3001
-const DATABASE_URL = process.env.DATABASE_URL || "UNDEFINED"
-
-app.get('/zagster', (request, Response) => {
-const pool= new Pool ( {
-    connectionString: DATABASE_URL:https://zagster-service.herokuapp.com/,
-})
-
-pool.query('SELECT * FROM rides LIMIT 1', (err, results) => {
-    Response.send(results.rows[0])
-    pool.end()
-})
-})
-
-// data
-app.get('/zagster', (request, Response) => {
-    const pool= new Pool ( {
-        connectionString: DATABASE_URL:https://zagster-service.herokuapp.com/,
-    })
-    
-    pool.query('SELECT * FROM rides', (err, results) => {
-        Response.send(results.rows[])
-        pool.end()
-    })
-    })
-        app.listen(PORT)
