469ba5863555417e4cd68d0b5a1ad481972d71db
Jed Hartley <hartleje@oregonstate.edu>
2019-11-20 15:49:58 -0800

    Visualization Project Milestone 2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+$(updateGraph)
 $(updateView)
 function updateView(){
     alert("Zagster Data")
@@ -13,4 +14,30 @@ function updateView() {
 function updateRideCount(data) {
   numberOfRides = data.count
   $("h2#rideCount").html(numberOfRides)
+}
+// fade out
+$(function() {
+  $('body').removeClass('fade-out');
+});
+// fade out
+function updateGraph(){
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
 }
